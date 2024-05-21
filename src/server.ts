0a0147baddcea06f285814d1c026e288e0e1510c
Jose Deyvid <j_deyvid@live.com>
2024-05-21 15:54:14 -0300

    commit

--- src/server.ts
+++ src/server.ts
@@ -7,21 +7,26 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
-const cors = require("cors");
-const corsOptions = {
-  origin: '*',
-  credentials: true,           
-  optionSuccessStatus: 200,
-}
+// const cors = require("cors");
+// const corsOptions = {
+//   origin: '*',
+//   credentials: true,           
+//   optionSuccessStatus: 200,
+// }
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
-app.use(cors(corsOptions)) 
+// app.use(cors(corsOptions)) 
 app.use(express.json())
 app.use(router)
 
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")))
 
 app.get("/", (req: Request, res: Response) => {
-  res.json("Express + TS Server");
+  res.send("Express + TS Server");
 })
 
 app.listen(port, () => {
